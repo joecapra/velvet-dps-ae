cd088091f97e4779215c2326edbffd1610002d78
Joe Capra <jcapra@160over90.com>
2020-08-29 10:32:32 -0700

    initial commit

--- src/client-src/comps/Upload.jsx
+++ src/client-src/comps/Upload.jsx
@@ -2,17 +2,30 @@
  * @author Tomer Riko Shalev
  */
 
-import React from 'react'
+import React from "react"
 
 export default class Upload extends React.Component {
+	constructor() {
+		super()
 
-    render() {
-        return (
-            <div>
-                <h1>Upload</h1>
-            </div>
-        )
+		this.state = {
+			data: [],
+		}
+	}
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((data) => this.setState({ data: data }))
+	}
 
-    }
-
+	render() {
+		return (
+			<div>
+				<h1>Upload</h1>
+				{this.state.data.map((item) => (
+					<h1>{item.name}</h1>
+				))}
+			</div>
+		)
+	}
 }
